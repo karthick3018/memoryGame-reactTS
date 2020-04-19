87734574b3b5513b947f2ffdcf0a01e20b3e2a7e
karthick3018 <karthickraja3018@gmail.com>
2020-04-19 23:39:30 +0530

    Updates

---
+++ precache-manifest.d403c5073766ac0d0c4bb587bf696fe2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6e7a8ba59435bfed6a573775c748d7",
+    "url": "/memoryGame-reactTS/index.html"
+  },
+  {
+    "revision": "f132b0b7cb3be5681335",
+    "url": "/memoryGame-reactTS/static/css/main.3e4d0c73.chunk.css"
+  },
+  {
+    "revision": "ca1188acd530143595f5",
+    "url": "/memoryGame-reactTS/static/js/2.7f3b7bef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/memoryGame-reactTS/static/js/2.7f3b7bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f132b0b7cb3be5681335",
+    "url": "/memoryGame-reactTS/static/js/main.8a5ec397.chunk.js"
+  },
+  {
+    "revision": "3db669dc7fdd78a20f06",
+    "url": "/memoryGame-reactTS/static/js/runtime-main.62b99c56.js"
+  },
+  {
+    "revision": "de9c7ed0c6b017ca86855264f5513bce",
+    "url": "/memoryGame-reactTS/static/media/default.de9c7ed0.jpg"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/memoryGame-reactTS/static/media/p1.0cded3a3.png"
+  },
+  {
+    "revision": "ed419638ef916666a989dfa868238b93",
+    "url": "/memoryGame-reactTS/static/media/p5.ed419638.png"
+  }
+]);
