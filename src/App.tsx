520114894e503df83a2e0a64ffa93514123e3105
karthick3018 <karthickraja3018@gmail.com>
2020-04-26 00:53:18 +0530

    styles changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import MemoryGame from './components';
+import './App.css'
 
 function App() {
   return (
     <>
-    <p>A Memory game for JS programmers </p>
+    <p className='text'>A Memory game for JS programmers </p>
      <MemoryGame/>
     </>
   );
